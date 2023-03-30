d7db7c14b535a20d153dd729234addf09bda860f
Tatiana Pauline Fernandes <79177501+pauline40-ss@users.noreply.github.com>
2023-03-30 08:41:38 -0300

    estudo de operadores

---
+++ bolleanos.py
@@ -0,0 +1,12 @@
+idade1 = 10
+idade2 = 15
+altura1 = 1.77
+altura2 = 1.65
+altura3 = altura2
+
+print(idade1 > idade2)
+print(idade1 <= idade2)
+print('Python'== 'python')
+print('banana'!= 'abacaxi')
+print(altura1 >= altura2)
+print(altura2 > altura3)
--- operadores.py
+++ operadores.py
@@ -1,1 +1,10 @@
-cls
+numero1 = 10
+numero2 = 30
+
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 // numero2)
+print(numero1**3)
+print(numero1 % numero2)
