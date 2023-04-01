78aaace34a107ebfbc0026b603b80b255dc77ff8
Tatiana Pauline Fernandes <79177501+pauline40-ss@users.noreply.github.com>
2023-04-01 15:38:45 -0300

    aprendendo while

---
+++ while.py
@@ -0,0 +1,30 @@
+#numero_sorteado = 15
+
+#numero_escolhido = int(input('Informe um número entre 1 e 20: '))
+
+#if numero_sorteado == numero_escolhido:
+
+#print('Você acertou!')
+#else:
+ #print('Você errou!')  
+    
+#altura_media =1.80
+    
+#altura_escolhida = float(input('Informe uma altura entre 1.60 e 1.80: '))
+    
+#while altura_media != altura_escolhida:
+    #print('Você errou o número, tente novamente ....')
+    
+    #altura_escolhida = float(input('Informe uma altura entre 1.60 e 1.80: '))
+    #print('Parabéns, você acertou!')
+    
+contador = 0
+
+while contador < 20:
+        print(contador)
+    
+contador = contador + 1
+    
+    
+
+
