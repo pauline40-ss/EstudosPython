a4532224284206358a284a21a70a6db4ea18b84d
Tatiana Pauline Fernandes <79177501+pauline40-ss@users.noreply.github.com>
2023-04-01 16:29:41 -0300

    estudo sobre o for

---
+++ for.py
@@ -0,0 +1,12 @@
+#for variavel in range(1,10):
+    #print(variavel)
+    
+    # como automatizar o processo
+soma = 0 
+    
+for i in range( 1,4 ):
+        nota = float(input(f'Informe a sua nota{i}: '))
+        
+        soma = soma + nota 
+        
+print(soma / 3)               
